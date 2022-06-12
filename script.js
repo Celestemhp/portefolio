0b252a92403d5a16c60b2c1de5b1addf8fb53bc9
Celeste Michelle <cele0216@stud.kea@purplegraphics.dk>
2022-06-13 01:08:23 +0200

    forside_virker

--- script.js
+++ script.js
@@ -6,19 +6,6 @@ document.querySelector("body").addEventListener("mousedown", click);
 function click() {
     console.log("lyd");
     document.querySelector("#klikLyd").play();
-    document.querySelector("#klikLyd").volume = 1.5;
 }
 
-document.querySelector("body").addEventListener("mousedown", click); {
-    if (click.pressure == 0) {
-      // No pressure
-      process_no_pressure(click);
-    } else if (click.pressure == 1) {
-      // Maximum pressure
-      process_max_pressure(click);
-    } else {
-      // Default
-      process_pressure(click);
-    }
-  }
 
